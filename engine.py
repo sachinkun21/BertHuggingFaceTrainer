3236159550060b0efeaf785177637ec918f13269
Sachin Kaushik <kausach@amazon.com>
2022-02-10 22:21:04 +0530

    Logging batch level info

--- engine.py
+++ engine.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 
 def train_fn(data_loader, model, optimizer, device, scheduler, epoch, wandb):
     # setting model to train mode
+    init_step = 1
     model.train()
     final_loss = 0
     step_size = len(data_loader)
@@ -17,16 +18,19 @@ def train_fn(data_loader, model, optimizer, device, scheduler, epoch, wandb):
         loss.backward()
         optimizer.step()
         scheduler.step()
+
         final_loss += loss.item()
 
-        # logging step level loss info
-        wandb.log({"TrainSteploss": final_loss})
+        # logging batch level loss info
+        wandb.log({"Average Train Batch loss": final_loss/init_step})
+        init_step += 1
 
     return final_loss/len(data_loader)
 
 
 def eval_fn(data_loader, model, device, epoch, wandb):
     # setting model to eval mode
+    init_step = 1
     model.eval()
     eval_loss = 0
     step_size = len(data_loader)
@@ -36,8 +40,9 @@ def eval_fn(data_loader, model, device, epoch, wandb):
         _, _, loss = model(**data)
         eval_loss += loss.item()
 
-        # logging step level loss info
-        wandb.log({"ValSteploss": eval_loss})
+        # logging batch level loss info
+        wandb.log({"Average Val Batch loss": eval_loss / init_step})
+        init_step += 1
 
     return eval_loss/len(data_loader)
 
